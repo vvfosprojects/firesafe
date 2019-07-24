b2748dfce1e5fc19dd02e9f66c3e97ed4d407a23
Marcello Esposito <esposito.marce@gmail.com>
2019-07-24 09:33:24 +0200

    Healthcheck handler
    E' presente il controller healthcheck.

---
+++ src/backend/WebApiGui/Controllers/HealthcheckController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Firesafe.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthcheckController : ControllerBase
+    {
+        // GET: api/Healthcheck
+        [HttpGet]
+        public object Get()
+        {
+            return new
+            {
+                State = "SUCCESS",
+                Description = string.Empty
+            };
+        }
+    }
+}
