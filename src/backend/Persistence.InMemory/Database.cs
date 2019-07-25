5d12dd16b121c161f2760a027617b2cfd46aeca6
matteonoccodev <52407199+matteonoccodev@users.noreply.github.com>
2019-07-25 10:17:33 +0200

    Formato Data per Prodotti
    Correzione problema relativo alla generazione della data (errore in fase di pubblicazione sul Server).

--- src/backend/Persistence.InMemory/Database.cs
+++ src/backend/Persistence.InMemory/Database.cs
@@ -12,7 +12,8 @@ namespace Persistence.InMemory
         {
             this.prodotti = new List<Prodotto>()
             {
-                new Prodotto() { Prog  =  "38628", CodiceOmol = "PS1045D20D1IM00018", Tipo = new Tipo { Nome =  "O"}, Ditta = "MORFEUS S.p.A.", Classe = new Classe {Nome = " 1 IM"}, Firma  =  DateTime.Parse ("27-06-2016") ,  Scadenza  = DateTime.Parse ("27-06-2021"),  Impiego  =  "MATERASSO",  MacroGruppo  =  "LETTI E DIVANI",  DenominazioneCommerciale  =  "SERIE LARA FR"},
+                new Prodotto() { Prog  =  "38628", CodiceOmol = "PS1045D20D1IM00018", Tipo = new Tipo { Nome =  "O"}, Ditta = "MORFEUS S.p.A.", Classe = new Classe {Nome = " 1 IM"}, Firma = new DateTime(2010, 8, 18) ,  Scadenza  = DateTime.Parse ("27-06-2021"),  Impiego  =  "MATERASSO",  MacroGruppo  =  "LETTI E DIVANI",  DenominazioneCommerciale  =  "SERIE LARA FR"},
+                new Prodotto() { Prog ="39377", CodiceOmol = "MI1481A70D200011", Tipo = new Tipo { Nome = "O"} , Ditta = "BIANCHI PAOLO S.r.l.", Classe = new Classe {Nome = " 2"}, Firma  =  new DateTime(2021, 12, 06), Scadenza = new DateTime(2016, 12, 06), Impiego = "STRUTTURE PRESSOSTATICHE, TENDONI, TELONI", MacroGruppo = "TELI E TENDAGGI", DenominazioneCommerciale = "BIATEX COIBENTATO"},
             };
         }
 
